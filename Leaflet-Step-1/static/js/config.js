b57c64d0f2ec44bd4971c44f6332b280a4481eae
Ken Yeh <yehitsken@gmail.com>
2021-04-23 16:40:19 -0700

    added initial map and index

---
+++ Leaflet-Step-1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoieWVoa2VuIiwiYSI6ImNrbjF6bGlyNjB6YWcycG8yNjE1NGluOGEifQ.rPr72ulCe9h8iC6c7kpHKQ";
---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,7 @@
+// Create map object
+var myMap = L.map("map", {
+    center: [33.98, -39.17],
+    zoom: 3
+  });
+
+
